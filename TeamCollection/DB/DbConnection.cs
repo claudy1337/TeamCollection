96a81d1377aee6eed1136dfb58bab11a9bb567e2
darkroot <45602451+claudy1337@users.noreply.github.com>
2022-03-04 15:31:12 +0300

    mongo db add

---
+++ TeamCollection/DB/DbConnection.cs
@@ -0,0 +1,11 @@
+﻿using MongoDB.Driver;
+
+namespace TeamCollection.DB
+{
+    public class DbConnection
+    {
+        private static string _connectionString = "mongodb://localhost:27017";
+        private static MongoClient _client = new MongoClient(_connectionString);
+        IMongoClient MongoClient = (IMongoClient)_client.GetDatabase("TeamCollection");
+    }
+}
